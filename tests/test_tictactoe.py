ef153a749282a5564d168b03ff6ffe3c0fd2aacb
azhadin <azhadin@opentext.com>
2024-08-22 21:23:11 +0800

    added more tests

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch
-from src.app import check_winner, is_board_full, get_move, play_game
+from io import StringIO
+from src.app import check_winner, is_board_full, get_move, play_game, print_board, exit_game
 
 class TestTicTacToe(unittest.TestCase):
 
@@ -79,6 +80,76 @@ class TestTicTacToe(unittest.TestCase):
         with unittest.mock.patch('builtins.input', side_effect=['10', '3']):
             row, col = get_move(board, player)
             self.assertEqual((row, col), (0, 2))
+            
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_board(self, mock_stdout):
+        board = [
+            ['X', 'O', ' '],
+            [' ', 'X', 'O'],
+            [' ', ' ', 'X']
+        ]
+        print_board(board)
+        expected_output = (
+            "X | O |  \n"
+            "-----------\n"
+            "  | X | O\n"
+            "-----------\n"
+            "  |   | X\n"
+            "-----------\n"
+        )
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_exit_game(self, mock_stdout):
+        with self.assertRaises(SystemExit):
+            exit_game()
+        self.assertIn("Exiting the game.", mock_stdout.getvalue())
+        
+    @patch('builtins.input', side_effect = ['1','2','5','6','9'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_playerX_wins(self, mock_stdout, mock_input):
+        with self.assertRaises(SystemExit):
+            play_game(size=3, num_players=2)
+        output = mock_stdout.getvalue()
+        self.assertIn("Player X wins!", output)
+        self.assertIn("Exiting the game.", output)
+    
+    @patch('builtins.input', side_effect = ['1','2','3','4','5','9','8','7','6'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_game_draw(self, mock_stdout, mock_input):
+        with self.assertRaises(SystemExit):
+            play_game(size=3, num_players=2)
+        output = mock_stdout.getvalue()
+        self.assertIn("It's a draw!", output)
+        self.assertIn("Exiting the game.", output)
+        
+    @patch('builtins.input', side_effect = ['0'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_exit_on_0_when_moving(self, mock_stdout, mock_input):
+        board = [
+            ['X', ' ', ' '],
+            [' ', ' ', ' '],
+            [' ', ' ', ' ']
+        ]
+        with self.assertRaises(SystemExit):
+            get_move(board, player='O')
+        output = mock_stdout.getvalue()
+        self.assertIn("Exiting the game.", output)
+        
+    #@patch('builtins.input', side_effect = ['1'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_check_already_taken_cell(self, mock_stdout):
+        board = [
+            ['X', 'X', 'O'],
+            ['O', 'O', 'X'],
+            [' ', ' ', ' ']
+        ]
+        with unittest.mock.patch('builtins.input', side_effect=['2', '3']):
+            row, col = get_move(board, player='O')
+            self.assertEqual((row, col), (1, 2))
+            output = mock_stdout.getvalue()
+            self.assertIn("This cell is already taken. Try another one.", output)
+        
 
 if __name__ == "__main__":
     unittest.main()
